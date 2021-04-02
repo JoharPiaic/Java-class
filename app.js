945a7b7719d2fe26d4a0b361ed2770fb6a15e3bb
shahzadali57 <shyzieli11@gmail.com>
2021-04-02 19:21:07 +0500

    Java class

---
+++ app.js
@@ -0,0 +1,41 @@
+// var a = 10;
+// if(a=a)
+// {
+//     alert(a);
+// }
+
+// else{
+//     alert(" no maoney"); 
+// }
+
+// // // //POP  last index delete krta ha 
+
+// var names = ['Johar','Asad','Saad',"Rafay"]
+// names.pop();
+// console.log(names);
+
+
+//   //  // SHIFT  First index delete krta ha 
+
+// var names = ['Johar','Asad','Saad', 'Rafay']
+// names.shift();
+// console.log(names);
+
+//   //  // UN SHIFT  First index SE ADD krta ha 
+
+// var names = ['Johar','Asad','Saad', 'Rafay','Hassan']
+// names.unshift(0,'ALI');
+// console.log(names);
+
+
+// //   //  // SPLICE delete given  index and show remaining index
+
+// var names = ['Johar','Asad','Saad','Rafay','Hassan']
+// names.splice(1,4);
+// console.log(names);
+
+//   //  // SLICE delete given  index and show remaining index
+
+var names = ['Johar','Asad','Saad','Rafay','Hassan']
+names.slice([0,2]);
+console.log(names);
